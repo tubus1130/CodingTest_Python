75557daef46adccda5caa76e9751710badc9aaed
김동호 <50819376+tubus1130@users.noreply.github.com>
2023-12-18 14:55:00 +0900

    Create Q1_모험가 길드.py

---
+++ Solved/그리디/Q1_모험가 길드.py
@@ -0,0 +1,15 @@
+#1. 모험가길드
+n = int(input())
+data = list(map(int, input().split()))
+
+data.sort()
+result = 0
+cnt = 0
+
+for i in data:
+  cnt += 1
+  if cnt >= i:
+    result += 1
+    cnt = 0
+
+print(result)
